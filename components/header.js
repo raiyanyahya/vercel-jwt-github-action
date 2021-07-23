dd6c34dc09b97c27f2d44ae75c332523e9a72ae3
Raiyan Yahya <raiyan.yahya@wirelesscar.com>
2021-07-23 22:57:56 +0200

    test prod deploy

--- components/header.js
+++ components/header.js
@@ -25,7 +25,7 @@ export default function Header () {
                   signIn()
                 }}
               >
-                Sign in
+                Sign in Now
               </a>
           </>}
           {session && <>
