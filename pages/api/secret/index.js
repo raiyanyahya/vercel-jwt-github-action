3eeb58272b5525493bdeb43ef950dbb149b40757
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2021-08-06 16:30:50 +0200

    Update index.js

--- pages/api/secret/index.js
+++ pages/api/secret/index.js
@@ -1,11 +1,11 @@
 import { getSession } from "next-auth/client";
-import Cookies from 'cookies'
+//import Cookies from 'cookies'
 
 export default async (req, res) => {
   const session = await getSession({ req });
-const cookies = new Cookies(req, res)
+//const cookies = new Cookies(req, res)
     // Get a cookie
-    console.log("my cokokie is",cookies.get('__Secure-next-auth.session-token'))
+    //console.log("my cokokie is",cookies.get('__Secure-next-auth.session-token'))
   if (session) {
     const res1 = await fetch("https://vercel-python-action.vercel.app/api",{
   method: 'POST',
