b81c5781e035595462598a2a9656f5146d6ace3e
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2021-08-07 03:02:22 +0200

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,10 +8,21 @@ const options = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  secret: process.env.SECRET,
+  
   session: {
     jwt: true
-  }
+  },
+  jwt: {
+  // A secret to use for key generation - you should set this explicitly
+  // Defaults to NextAuth.js secret if not explicitly specified.
+  // This is used to generate the actual signingKey and produces a warning
+  // message if not defined explicitly.
+  secret: process.env.SECRET,
+  // You can generate a signing key using `jose newkey -s 512 -t oct -a HS512`
+  // This gives you direct knowledge of the key used to sign the token so you can use it
+  // to authenticate indirectly (eg. to a database driver)
+  signingKey: {"kty":"oct","kid":"70QWdXe9gG-nwvZdmNyr_94ONoLRmGq_X45z1Cune88","alg":"HS512","k":"NRNYrTJLRQEtiOx2NxoqDYuvWIXqvdtwi3lZ8eZYv8KnG0Ph3WwNgqqLqdzI9BviYUIXRFAqkm4v-mpl08qudg"},
+}
 };
 
 export default (req, res) => NextAuth(req, res, options);
