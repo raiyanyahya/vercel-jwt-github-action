280159e1d5d2b45ef4a9be004ec6c4e6e9e72abb
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2021-08-09 11:16:31 +0200

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,19 +1,11 @@
 import Providers from "next-auth/providers";
 import NextAuth from 'next-auth';
-
+import jwt from "jsonwebtoken";
 const options = {
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      profile(profile) {
-        return {
-          id: profile.id.toString(),
-          name: profile.name || profile.login,
-          email: profile.email,
-          image: profile.avatar_url,
-        }
-      },
     }),
   ],
   secret: process.env.SECRET,
@@ -21,22 +13,45 @@ const options = {
     jwt: true
   },
   jwt: {
-  // A secret to use for key generation - you should set this explicitly
-  // Defaults to NextAuth.js secret if not explicitly specified.
-  // This is used to generate the actual signingKey and produces a warning
-  // message if not defined explicitly.
-  secret: process.env.SECRET,
-  // You can generate a signing key using `jose newkey -s 512 -t oct -a HS512`
-  // This gives you direct knowledge of the key used to sign the token so you can use it
-  // to authenticate indirectly (eg. to a database driver)
-    signingKey: {
-     kty: "oct",
-     kid: "Dl893BEV-iVE-x9EC52TDmlJUgGm9oZ99_ZL025Hc5Q",
-     alg: "HS512",
-     k: "K7QqRmJOKRK2qcCKV_pi9PSBv3XP0fpTu30TP8xn4w01xR3ZMZM38yL2DnTVPVw6e4yhdh0jtoah-i4c_pZagA"
+    // A secret to use for key generation (you should set this explicitly)
+    secret: process.env.SECRET,
+    encode: async ({ secret, token, maxAge }) => {
+      const jwtClaims = {
+        "sub": token.sub.toString() ,
+        "name": token.name ,
+        "email": token.email,
+        "iat": Date.now() / 1000,
+        "exp": Math.floor(Date.now() / 1000) + (24*60*60),
+      };
+      const encodedToken = jwt.sign(jwtClaims, secret, { algorithm: 'HS256'});
+      return encodedToken;
+    },
+    decode: async ({ secret, token, maxAge }) => {
+      const decodedToken = jwt.verify(token, secret, { algorithms: ['HS256']});
+      return decodedToken;
+    },
   },
+  callbacks: {
+    // async signIn(user, account, profile) { return true },
+    // async redirect(url, baseUrl) { return baseUrl },
+    async session(session, token) { 
+      const encodedToken = jwt.sign(token, process.env.SECRET, { algorithm: 'HS256'});
+      session.id = token.id;
+      session.token = encodedToken;
+      return Promise.resolve(session);
+    },
+    async jwt(token, user, account, profile, isNewUser) { 
+      const isUserSignedIn = user ? true : false;
+      // make a http call to our graphql api
+      // store this in postgres
+      if(isUserSignedIn) {
+        token.id = user.id.toString();
+      }
+      return Promise.resolve(token);
+    }
+  },
+  debug: true,
   //signingKey: {"kty":"oct","kid":"70QWdXe9gG-nwvZdmNyr_94ONoLRmGq_X45z1Cune88","alg":"HS512","k":"NRNYrTJLRQEtiOx2NxoqDYuvWIXqvdtwi3lZ8eZYv8KnG0Ph3WwNgqqLqdzI9BviYUIXRFAqkm4v-mpl08qudg"},
-}
 };
 
 export default (req, res) => NextAuth(req, res, options);
